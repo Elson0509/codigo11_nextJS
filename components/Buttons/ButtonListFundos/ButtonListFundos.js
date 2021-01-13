6fedc69296d81e7b7e99ced4855428fdca601e34
Elson Ramos <elson.0509@gmail.com>
2021-01-12 22:03:08 -0500

    adding text after click in button list fiis in landing page

--- components/Buttons/ButtonListFundos/ButtonListFundos.js
+++ components/Buttons/ButtonListFundos/ButtonListFundos.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Link from 'next/link'
 import classes from './ButtonListFundos.module.css'
 const ButtonListFundos = () => {
+    const [btnText, setBtnText] = useState('Lista de Fundos')
+
     return (
         <div className={`${classes.Container_btn_list} mb-5`}>
             <Link href='/lista'>
-                <a className={`${classes.Btn_lista} ${classes.Effect01}`}>
-                    Lista de Fundos
+                <a onClick={()=>{setBtnText('Listando...')}} className={`${classes.Btn_lista} ${classes.Effect01}`}>
+                    {btnText}
                 </a>
             </Link>
         </div>
